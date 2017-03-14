796d51adec20a9ccfa9be7a5873bc1a302488187
dummied <dummied@gmail.com>
2017-03-14 16:37:08 -0400

    removes weird-ass post

--- app/models/post.rb
+++
@@ -1,6 +0,0 @@
-class Post < ApplicationRecord
-  belongs_to :user
-
-  validates :body, length: {maximum: 160}
-
-end
--- db/migrate/20170306170949_create_posts.rb
+++ db/migrate/20170306170949_create_posts.rb
@@ -1,6 +1,6 @@
 class CreatePosts < ActiveRecord::Migration[5.0]
   def change
-    create_table :posts do |t|
+    create_table :chirps do |t|
       t.string :body
       t.references :user, foreign_key: true
 
